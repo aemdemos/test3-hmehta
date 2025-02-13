1201bd764e980fea0fd90bf90e65b83fbbff5f96
Abhishek Garg <abhigarg@adobe.com>
2025-02-13 13:40:27 +0530

    side-nav commiting header footer changes

--- templates/news-landing/news-landing.js
+++ templates/news-landing/news-landing.js
@@ -2,31 +2,8 @@ import { getMetadata, buildBlock, loadBlock, decorateBlock } from '../../scripts
 import { div, h1 } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
-  // Find breadcrumb section if it exists
-//   const breadcrumb = block.querySelector('.breadcrumb-container');
-//   const breadcrumbTitle = getMetadata('breadcrumb-title');
    loadNews(block);
-//   breadcrumb.classList.add('grey-background');
-//   breadcrumb.appendChild(div({ class: 'default-content-wrapper' }, h1(breadcrumbTitle)));
-  const outDiv = div();
 
-  // Move all sections to wrapper div except breadcrumb
-//   const sections = block.querySelectorAll('.section');
-//   sections.forEach((section) => {
-//     if (!section.classList.contains('breadcrumb-container')) {
-//       outDiv.appendChild(section);
-//     }
-//   });
-
-  // console.log(output);
-
-  // Clear the block and add breadcrumb (if exists) and wrapper div
-  block.innerHTML = '';
-//   if (breadcrumb) {
-//     block.appendChild(breadcrumb);
-//   }
-
-  block.appendChild(outDiv);
 }
 
 
